0286d145eb1aa87a5f770c906c6ba3d7ca8de4a2
Lukas Strickler <145582491+LukasStrickler@users.noreply.github.com>
2024-12-04 11:10:14 +0100

    fix(report): fixed build

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
 import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
-import { reportRouter } from "./routers/report"
+import { reportRouter } from "./routers/report";
 
 /**
  * This is the primary router for your server.
