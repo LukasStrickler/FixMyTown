9344b68454bec4f00c77136e1302c9d82a0605e1
Lukas Strickler <145582491+LukasStrickler@users.noreply.github.com>
2024-12-12 15:18:32 +0100

    feat(reports): Add report map component and fetch reports

--- src/app/[lang]/myReports/page.tsx
+++ src/app/[lang]/myReports/page.tsx
@@ -1,22 +1,24 @@
 import { auth } from "@/server/auth";
-import { HydrateClient } from "@/trpc/server";
-// import { getDictionary } from "../../../get-dictionary";
+import { api, HydrateClient } from "@/trpc/server";
+import { getDictionary } from "../../../get-dictionary";
 import { type Locale } from "@/i18n-config";
-
+import ReportMap from "@/components/ReportOverview/Map/report-map";
+import { metadata } from "../layout";
+import { reports } from "@/server/db/schema/reports";
 export default async function MyReports({
   params: { lang },
 }: {
   params: { lang: Locale };
 }) {
-
+  const dictionary = await getDictionary(lang);
   const session = await auth();
-
+  const { reports, metadata } = await api.report.getReports.query({
+    userId: session?.user.id,
+  });
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center ">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <div>{JSON.stringify(session)}</div>
-        </div>
+        <ReportMap dictionary={dictionary} metadata={metadata} reports={reports} />
       </main>
     </HydrateClient>
   );
