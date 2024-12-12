688ad08c169d3e4e36c8fd3c47d8b3d47c1ef565
timbo003 <tim.strohmenger@tisito.de>
2024-12-12 15:39:08 +0100

    feat: implement user role update functionality
    Adds a new hook `useUpdateUserRole` to manage user role updates 
    through the API. Integrates this hook into the user administration 
    dropdown menu, replacing alert prompts with actual role update 
    calls. Introduces a new `userRouter` for handling role updates 
    on the server side, ensuring a structured approach to user 
    management.
    
    
    promoting works but table is not updating

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { reportRouter } from "./routers/report";
+import { userRouter } from "./routers/userRouter";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { reportRouter } from "./routers/report";
 export const appRouter = createTRPCRouter({
   post: postRouter,
   report: reportRouter,
+  user: userRouter,
 });
 
 // export type definition of API
