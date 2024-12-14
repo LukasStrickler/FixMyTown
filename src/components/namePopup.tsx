1c448320feab7500d7e41059ad9a94426f44c0cb
i568830 <joscha.staehle@sap.com>
2024-12-14 21:35:36 +0100

    implement colorscheme

--- src/components/namePopup.tsx
+++ src/components/namePopup.tsx
@@ -8,12 +8,10 @@ import { useSession } from "next-auth/react";
 export default function NamePopup({ userName }: { userName: string | null }) {
   const [showPopup, setShowPopup] = useState(false);
 
-  const utils = api.useUtils();
   const { data: session } = useSession();
   const updateUserName = api.user.updateUserName.useMutation();
   const { toast } = useToast();
 
-  // Get user ID safely
   const userId = session?.user?.id;
 
   const handleUpdateName = async () => {
@@ -49,12 +47,14 @@ export default function NamePopup({ userName }: { userName: string | null }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-gray-800 p-6 rounded-lg shadow-lg text-center">
-        <h2 className="text-2xl font-semibold text-white">Welcome!</h2>
-        <p className="text-white mt-2">Your name is not set. Would you like to set it to "Maximilian"?</p>
+      <div className="bg-background p-6 rounded-lg shadow-lg text-center">
+        <h2 className="text-2xl font-semibold text-foreground">Welcome!</h2>
+        <p className="text-foreground mt-2">
+          Your name is not set. Would you like to set it to "Maximilian"?
+        </p>
         <button
           onClick={handleUpdateName}
-          className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-800"
+          className="mt-4 bg-primary text-white px-4 py-2 rounded hover:bg-primary/50 transition"
         >
           Set Name to Maximilian
         </button>
