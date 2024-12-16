81ed8cf407175e1f9cf761700179ff49fba89b04
Lukas Strickler <145582491+LukasStrickler@users.noreply.github.com>
2024-12-16 21:44:07 +0100

    feat(reports-table): add worker prop to ReportsTable component

--- src/components/ReportOverview/Table/reports-table.tsx
+++ src/components/ReportOverview/Table/reports-table.tsx
@@ -1,18 +1,19 @@
 "use client"
 
-import { columns, ReportData } from "./columns"
+import { columns, type ReportData } from "./columns"
 import { DataTable } from "./data-table"
 import type { Dictionary } from "@/dictionaries/dictionary"
 
 interface ReportsTableProps {
     reports: ReportData[]
     dictionary: Dictionary
+    worker: boolean
 }
 
-export function ReportsTable({ reports, dictionary }: ReportsTableProps) {
+export function ReportsTable({ reports, dictionary, worker }: ReportsTableProps) {
     return (
         <DataTable
-            columns={columns(dictionary)}
+            columns={columns(dictionary, worker)}
             data={reports}
             dictionary={dictionary}
         />
