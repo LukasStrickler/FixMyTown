04b73a676f5f79a7d0688ff4f63c3160ab8572f5
Lukas Strickler <145582491+LukasStrickler@users.noreply.github.com>
2024-12-16 21:43:54 +0100

    feat(reports): add worker mode to reports table and map

--- src/app/[lang]/worker/report/MapView/page.tsx
+++ src/app/[lang]/worker/report/MapView/page.tsx
@@ -10,6 +10,9 @@
 import { auth } from "@/server/auth";
 import { HydrateClient } from "@/trpc/server";
 import { type Locale } from "@/i18n-config";
+import ReportMap from "@/components/ReportOverview/Map/report-map";
+import { api } from "@/trpc/server"
+import { getDictionary } from "@/get-dictionary";
 
 export default async function MyReports({
     params: { lang },
@@ -18,12 +21,14 @@ export default async function MyReports({
 }) {
 
     const session = await auth();
+    const { reports } = await api.report.getWorkerReports();
+    const dictionary = await getDictionary(lang)
 
     return (
         <HydrateClient>
-            <main className="flex min-h-screen flex-col items-center justify-center ">
-                <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-                    <div>{JSON.stringify(session)}</div>
+            <main className="flex min-h-screen flex-col items-center justify-center">
+                <div className="w-full p-4">
+                    <ReportMap reports={reports} dictionary={dictionary} worker={true} />
                 </div>
             </main>
         </HydrateClient>
