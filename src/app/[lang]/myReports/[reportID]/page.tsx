c6d360a68967f79180898e8654ac5c7d9c2f96b7
timbo003 <tim.strohmenger@tisito.de>
2024-12-18 16:32:16 +0100

    move transformation logic out of return block

--- src/app/[lang]/myReports/[reportID]/page.tsx
+++ src/app/[lang]/myReports/[reportID]/page.tsx
@@ -1,13 +1,3 @@
-// fetches report data from the database based on the reportID slug
-// shows ReportDetails component but with restrited access to the user
-
-// only allows own reports to be viewed
-//  shows error if not found / not authorized
-// only allows some information to be shown
-//  no priority information etc
-// only allows some actions to be taken
-//  delete only when in status 1
-
 import { auth } from "@/server/auth";
 import { api, HydrateClient } from "@/trpc/server";
 import { type Locale } from "@/i18n-config";
@@ -37,19 +27,22 @@ export default async function MyReports({
         return <div>Report not found</div>;
     }
 
+    // Transform report data
+    const transformedReport = {
+        ...data,
+        protocolls: data.protocolls.map(protocol => ({
+            ...protocol,
+            status: protocol.status ?? 0 
+        }))
+    };
+
     return (
         <HydrateClient>
             <main className="flex min-h-screen flex-col items-center justify-center">
                 <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
                     <ReportDetails
                         dictionary={dictionary}
-                        report={{
-                            ...data,
-                            protocolls: data.protocolls.map(protocol => ({
-                                ...protocol,
-                                status: protocol.status ?? 0 // Default to 0 if null
-                            }))
-                        }}
+                        report={transformedReport}
                         worker={false}
                     />
                 </div>
