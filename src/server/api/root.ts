73cc1b98fce125f69b4b5ad5b91993a46762ba4c
Lukas Strickler <145582491+LukasStrickler@users.noreply.github.com>
2025-01-04 14:34:12 +0100

    feat(api): remove unused post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { reportRouter } from "./routers/report";
 import { userRouter } from "./routers/userRouter";
@@ -10,7 +9,6 @@ import { reportDetailsRouter } from "./routers/reportDetails";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   report: reportRouter,
   user: userRouter,
   reportDetails: reportDetailsRouter,
